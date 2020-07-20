3f75499c231a136d1f2c71ea1d33a52c1e069616
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-07-20 15:12:28 -0700

    Wrote init, ram_read, ram_write, load, and run functions without branch table.

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,6 +5,16 @@
 import sys
 from cpu import *
 
+print(sys.argv)
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    print("Must provide command line argument")
+    sys.exit()
+print("FILENAME:",filename)
+    
+print(filename)
+
 cpu = CPU()
 
 cpu.load()
